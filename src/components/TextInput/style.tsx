814a8a55776d00976ec995f4f392677c9cddbcf4
blacktoast <dbstjd100@naver.com>
2022-09-03 16:23:09 +0900

    Feat/ TextInput 오류 컴포넌트 스타일 구현

--- src/components/TextInput/style.tsx
+++ src/components/TextInput/style.tsx
@@ -7,6 +7,20 @@ export const TextInput = styled.input`
   border-radius: 12px;
 `;
 
+export const ErrorTextInput = styled.input`
+  margin-top: 8px;
+  padding: 16px;
+  background-color: #fdedee;
+  border-radius: 12px;
+`;
+
+export const ErrorComment = styled.span`
+  margin-top: 8px;
+  font-weight: 400;
+  font-size: 13px;
+  color: #ed4e5c;
+`;
+
 export const Label = styled.label`
   font-weight: 700;
   font-size: 13px;
