4975711cc4bd34291d3a2e30bfeaecd78511d7a8
blacktoast <dbstjd100@naver.com>
2022-09-05 20:29:52 +0900

    Feat/ userState 전역 상태

---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+import { atom } from 'recoil';
+
+export const userState = atom({
+  key: 'user', // unique ID (with respect to other atoms/selectors)
+  default: {
+    id: '',
+    name: '',
+  }, // default value (aka initial value)
+});
