e9f5e48997320ca9855e60062cb6f55143cf1385
blacktoast <dbstjd100@naver.com>
2022-09-06 00:15:13 +0900

    Refactor/ 상수값 constants파일로 이동

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,9 +8,7 @@ import { useRecoilState } from 'recoil';
 import { userState } from '../store/index';
 import { useRouter } from 'next/router';
 import { Header } from '../components/Header';
-
-const ERROR_INPUT_ID = '올바른 아이디 형식으로 입력해주세요.';
-const ERROR_INPUT_PASSWORD = '올바른 비밀번호 형식으로 입력해주세요.';
+import { ERROR_INPUT_ID, ERROR_INPUT_PASSWORD } from '../utilities/constants';
 
 const LoginPage: NextPage = () => {
   const [id, setId] = useState('');
