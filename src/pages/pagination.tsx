7ddb7ab60f496c03c4f04af4f3c4b228734b2d33
blacktoast <dbstjd100@naver.com>
2022-09-07 22:26:54 +0900

    Feat/ 없는 페이지 접근시 error 컴포넌트 출력

--- src/pages/pagination.tsx
+++ src/pages/pagination.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 import type { NextPage } from 'next';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import products from '../api/data/products.json';
@@ -10,36 +9,42 @@ import Pagination from '../components/Pagination';
 import { Header } from '../components';
 import { getProducts } from '../apis/products';
 import { usePagination } from '../hooks/usePagination';
+import { ERROR_NOTFOUND_PRODUCT_PAGE } from '../utilities/constants';
+import { Error } from '../components/Error';
 
-// 프로덕트 정보는 해당 페이지에서 가지기
-//
 const PaginationPage: NextPage = () => {
   const router = useRouter();
   const page = Number(router.query.page);
-  const [currentPage, pages, setCurrentPage, setTotalCount] = usePagination({});
+  const [currentPage, pages, totalPage, setCurrentPage, setTotalCount] = usePagination({});
+  const [error, setError] = useState('');
   useEffect(() => {
     const getData = async () => {
       try {
         if (page) {
           const data = await getProducts({ page });
+          setError('');
           setTotalCount(data.totalCount);
           setCurrentPage(page);
         }
       } catch (error) {
+        setError(ERROR_NOTFOUND_PRODUCT_PAGE);
         console.log(error);
       }
     };
-
     getData();
   }, [page, setCurrentPage, setTotalCount]);
 
   return (
     <>
       <Header />
-      <Container>
-        <ProductList products={products.slice(0, 10)} />
-        <Pagination currentPage={currentPage} pages={pages} />
-      </Container>
+      {error.length !== 0 ? (
+        <Error title={error} />
+      ) : (
+        <Container>
+          <ProductList products={products.slice(0, 10)} />
+          <Pagination currentPage={currentPage} pages={pages} totalPage={totalPage} />
+        </Container>
+      )}
     </>
   );
 };
--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,3 +1,4 @@
 export const ONE_HOURS_MILLISECONDS = 1000 * 60 * 60;
 export const ERROR_INPUT_ID = '올바른 아이디 형식으로 입력해주세요.';
 export const ERROR_INPUT_PASSWORD = '올바른 비밀번호 형식으로 입력해주세요.';
+export const ERROR_NOTFOUND_PRODUCT_PAGE = '존재하지 않는 페이지입니다';
