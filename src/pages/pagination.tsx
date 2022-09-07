b81c9e536452274db9efda6e34a9db7a59096c21
blacktoast <dbstjd100@naver.com>
2022-09-07 17:31:04 +0900

    Feat/ pagintion 페이지 usePagination 훅사용 해서 로직 변경

--- src/pages/pagination.tsx
+++ src/pages/pagination.tsx
@@ -1,47 +1,50 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 import products from '../api/data/products.json';
 import ProductList from '../components/ProductList';
 import Pagination from '../components/Pagination';
+import { Header } from '../components';
+import { getProducts } from '../apis/products';
+import { usePagination } from '../hooks/usePagination';
 
+// 프로덕트 정보는 해당 페이지에서 가지기
+//
 const PaginationPage: NextPage = () => {
   const router = useRouter();
-  const { page } = router.query;
+  const page = Number(router.query.page);
+  const [currentPage, pages, setCurrentPage, setTotalCount] = usePagination({});
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        if (page) {
+          const data = await getProducts({ page });
+          setTotalCount(data.totalCount);
+          setCurrentPage(page);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, [page, setCurrentPage, setTotalCount]);
 
   return (
     <>
-      <Header>
-        <Link href='/'>
-          <Title>HAUS</Title>
-        </Link>
-        <Link href='/login'>
-          <p>login</p>
-        </Link>
-      </Header>
+      <Header />
       <Container>
         <ProductList products={products.slice(0, 10)} />
-        <Pagination />
+        <Pagination currentPage={currentPage} pages={pages} />
       </Container>
     </>
   );
 };
 
 export default PaginationPage;
-
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-`;
-
-const Title = styled.a`
-  font-size: 48px;
-`;
 
 const Container = styled.div`
   display: flex;
