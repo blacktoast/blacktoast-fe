236dbe7ddc77906833cec6dd0808f8168a916ec4
blacktoast <dbstjd100@naver.com>
2022-09-07 22:27:11 +0900

    Feat/ totalPage 수를 반환하도록 로직 변경

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -3,27 +3,35 @@ import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 export const usePagination = ({
   perPage = 10,
   perPagesRange = 5,
-}): [number, number[], Dispatch<SetStateAction<number>>, Dispatch<SetStateAction<number>>] => {
+}): [
+  number,
+  number[],
+  number,
+  Dispatch<SetStateAction<number>>,
+  Dispatch<SetStateAction<number>>
+] => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pages, setPages] = useState([1, 2, 3, 4, 5]);
   const [totalCount, setTotalCount] = useState(0);
+  const [totalPage, setTotalPage] = useState(0);
 
   useEffect(() => {
     const totalPage = Math.ceil(totalCount / perPage);
+    setTotalPage(totalPage);
     const isLastPage = currentPage % perPagesRange === 0;
     const perPageStart = isLastPage
       ? (Math.floor(currentPage / perPagesRange) - 1) * perPagesRange
       : Math.floor(currentPage / perPagesRange) * perPagesRange;
-    // 마지지막 페이지가 토탈 페이지 보다 크다면 마지막-total해서 해당값이 range 범위로 가져가기
     const pageRange =
       totalPage - perPageStart < perPagesRange ? totalPage - perPageStart : perPagesRange;
 
     const tmpPages = new Array(pageRange).fill(0).map((_, i) => {
-      return i + 1 + perPageStart;
+      const pageIndex = i + 1;
+      return pageIndex + perPageStart;
     });
 
     setPages(tmpPages);
   }, [currentPage, perPage, perPagesRange, totalCount]);
 
-  return [currentPage, pages, setCurrentPage, setTotalCount];
+  return [currentPage, pages, totalPage, setCurrentPage, setTotalCount];
 };
