84300dc7f3160c3f000426e62b8ce36d35ce7afa
blacktoast <dbstjd100@naver.com>
2022-09-08 18:30:24 +0900

    Feat/ 무한스크롤 전용 state 구현

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { atom } from 'recoil';
+import { Product } from '../types/product';
 
 export const userState = atom({
   key: 'user', // unique ID (with respect to other atoms/selectors)
@@ -7,4 +8,21 @@ export const userState = atom({
     name: '',
     token: '',
   }, // default value (aka initial value)
+});
+
+type InfinityState = {
+  currentPage: number;
+  products: Product[];
+  offsetY: number;
+  totalCount: number;
+};
+
+export const infinityState = atom<InfinityState>({
+  key: 'infinity',
+  default: {
+    currentPage: 0,
+    products: [],
+    offsetY: 0,
+    totalCount: 0,
+  },
 });
