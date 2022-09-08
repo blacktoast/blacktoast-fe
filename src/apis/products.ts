d95868406a88a9f4b9efbe7a8d8cd7c1283e5959
blacktoast <dbstjd100@naver.com>
2022-09-09 01:44:58 +0900

    Feat/ 제품상세 get api 로직 구현

--- src/apis/products.ts
+++ src/apis/products.ts
@@ -11,7 +11,19 @@ export const getProducts = async ({ page = 1, size = 10 }) => {
     const data = await apiClient.get<Products>(`/products?page=${page}&size=${size}`);
     return data.data;
   } catch (error) {
-    console.log('err', error);
+    return Promise.reject(error);
+  }
+};
+
+type getProductType = {
+  id: number;
+};
+
+export const getProduct = async ({ id }: getProductType) => {
+  try {
+    const data = await apiClient.get<Products>(`/products/${id}`);
+    return data.data;
+  } catch (error) {
     return Promise.reject(error);
   }
 };
