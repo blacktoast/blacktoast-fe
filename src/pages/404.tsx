73fd80c5e62780dcf891759095f0794ea9150f3f
blacktoast <dbstjd100@naver.com>
2022-09-08 22:29:26 +0900

    Feat/ 에러페이지 구현

---
+++ src/pages/404.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import { Header } from '../components/';
+import { useRouter } from 'next/router';
+import { Error } from '../components';
+import {
+  ERROR_NOTFOUND_PRODUCTS_DETAIL_PAGE,
+  ERROR_NOTFOUND_PRODUCTS_PAGE,
+} from '../utilities/constants';
+
+type errorType = {
+  [key: string]: string;
+};
+const ProductDetailPage: NextPage = () => {
+  const [errorMassage, setErrorMessage] = useState('');
+
+  const router = useRouter();
+  useEffect(() => {
+    const errorPageType = router.asPath.split('/')[1].replace(/\?.*/, '');
+    console.log(router.asPath, errorPageType);
+    const errorMessageByType: errorType = {
+      products: ERROR_NOTFOUND_PRODUCTS_DETAIL_PAGE,
+      pagination: ERROR_NOTFOUND_PRODUCTS_PAGE,
+    };
+
+    setErrorMessage(
+      errorMessageByType[errorPageType] || '길을 잃었습니다 메인 페이지로 이동해주세요'
+    );
+  }, [router]);
+
+  return (
+    <>
+      <Header />
+      <Error title={errorMassage} />
+    </>
+  );
+};
+
+export default ProductDetailPage;
--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,4 +1,5 @@
 export const ONE_HOURS_MILLISECONDS = 1000 * 60 * 60;
 export const ERROR_INPUT_ID = '올바른 아이디 형식으로 입력해주세요.';
 export const ERROR_INPUT_PASSWORD = '올바른 비밀번호 형식으로 입력해주세요.';
-export const ERROR_NOTFOUND_PRODUCT_PAGE = '존재하지 않는 페이지입니다';
+export const ERROR_NOTFOUND_PRODUCTS_PAGE = '존재하지 않는 페이지입니다';
+export const ERROR_NOTFOUND_PRODUCTS_DETAIL_PAGE = '존재하지 않는 상품입니다';
